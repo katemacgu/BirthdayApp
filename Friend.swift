b6a27ce7f5e923290a780fcddf31f1527015a823
Kate <kmc.macguire@gmail.com>
2025-07-25 11:35:25 -0600

    added bday app

--- Friend.swift
+++ Friend.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date){
+    self.name = name
+    self.birthday = birthday
+    }
+}
